7db127d2658c791ca984f516d64c19237ba40fb1
Philipp Gampe <tastycrysis@gmail.com>
2025-01-14 11:26:51 +0100

    add form reload

--- src/Components/ColorForm/ColorForm.jsx
+++ src/Components/ColorForm/ColorForm.jsx
@@ -1,22 +1,29 @@
 import ColorInput from "../ColorInput/ColorInput";
 import "./ColorForm.css";
 import { nanoid } from "nanoid";
+import { useState, useEffect } from "react";
 
 export default function ColorForm({
   onSubmitColor,
   initialData = { role: "some color", hex: "#ff4a11", contrastText: "#ffffff" },
 }) {
+  const [key, setKey] = useState(0);
+
   function handleSubmit(event) {
     event.preventDefault();
     const formData = new FormData(event.target);
     const data = Object.fromEntries(formData);
     data.id = nanoid();
     onSubmitColor(data);
-    event.target.reset();
+    setKey((prevKey) => prevKey + 1);
   }
 
+  useEffect(() => {
+    console.log(key);
+  }, [key]);
+
   return (
-    <form onSubmit={handleSubmit}>
+    <form key={key} onSubmit={handleSubmit}>
       <label htmlFor="role">
         Role{" "}
         <input
@@ -27,11 +34,15 @@ export default function ColorForm({
         />
       </label>
       <label htmlFor="hex">
-        Hex <ColorInput id="hex" defaultValue={initialData.hex} />
+        Hex <ColorInput id="hex" name="hex" defaultValue={initialData.hex} />
       </label>
       <label htmlFor="contrastText">
         Contrast Text{" "}
-        <ColorInput id="contrastText" defaultValue={initialData.contrastText} />
+        <ColorInput
+          id="contrastText"
+          name="contrastText"
+          defaultValue={initialData.contrastText}
+        />
       </label>
       <button type="submit">Add Color</button>
     </form>
